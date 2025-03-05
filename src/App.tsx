2f9bbbbdfb33eaf991627da5ec930c36e71099cf
Marco van Harten <marcovanharten89@gmail.com>
2025-03-05 08:48:55 +0100

    feat: :sparkles: :construction: place editor in center

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { InlineEditor } from '@src/components/inline-editor'
+import './styles/global.css'
+
 function App() {
-  return <div>Hello world</div>
+  return (
+    <main className="max-w-xl mx-auto h-svh grid place-items-center">
+      <div className="w-full px-4 py-3 *:last:outline flex flex-col gap-2">
+        <label htmlFor="editor">Input</label>
+        <InlineEditor id="editor" />
+      </div>
+    </main>
+  )
 }
 
 export default App
